e3b694bf8844e155ced7833bdd2767fdd820d9ef
Yves Vindevogel <yves.vindevogel@asynchrone.com>
2021-01-29 16:30:07 +0100

    Verwijdering Sphinx

--- source/conf.py
+++ source/conf.py
@@ -59,6 +59,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_show_sphinx = False
+
 # -- Options for Latex/PDF output --------------------------------------------
 
 latex_toplevel_sectioning = 'part'
